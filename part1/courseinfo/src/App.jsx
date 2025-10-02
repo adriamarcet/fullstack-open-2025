8050651df9dc6c24fa790cf43209951a1cfd0873
Adrià <adriamarcetrovira@gmail.com>
2025-10-02 11:02:01 +0200

    Part1 Exercise 1.5: Change the course and its parts into a single JavaScript object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,27 +3,29 @@ import Content from "./Content.jsx";
 import Total from "./Total.jsx";
 
 function App() {
-  const course = 'Half Stack application development'
-  const parts = [{
-    name: 'Fundamentals of React',
-    exercises: 10 
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [{
+      name: 'Fundamentals of React',
+      exercises: 10 
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }]
+  };
 
   return (
     <>
-      <Header course={course} />
+      <Header course={course.name} />
       <Content 
-        parts={parts}
+        parts={course.parts}
       />
-      <Total parts={parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
