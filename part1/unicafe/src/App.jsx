e5778f3e70492e67d3771b2b098c436c2383a3d3
Adrià <adriamarcetrovira@gmail.com>
2025-10-06 12:48:39 +0200

    Part1 Exercise 1.6: unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,21 +5,25 @@ function App() {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   
+  const total = good + neutral + bad;
+  const average = total === 0 ? 0 : ((good - bad) / total).toFixed(2);
+  const positivePercentage = total === 0 ? 0 : ((good / total) * 100).toFixed(1);
+
   const handleGoodFeedback = () => {
     const updatedGood = good + 1;
-    return setGood(updatedGood);
+    setGood(updatedGood)
   }
   
   const handleNeutralFeedback = () => {
     const updatedNeutral = neutral + 1;
-    return setNeutral(updatedNeutral);
+    setNeutral(updatedNeutral);
   }
   
   const handleBadFeedback = () => {
     const updatedBad = bad + 1;
-    return setBad(updatedBad);
+    setBad(updatedBad)
   }
-  
+
   return (
     <>
       <h1>Give feedback to Unicafe</h1>
@@ -48,6 +52,9 @@ function App() {
           <li>Good: {good}</li>
           <li>Neutral: {neutral}</li>
           <li>Bad: {bad}</li>
+          <li>the total number of collected feedback: {total} </li>
+          <li>the average score: {average}</li>
+          <li>the percentage of positive feedback: {positivePercentage}%</li>
         </ul>
       </section>
     </>
@@ -55,4 +62,3 @@ function App() {
 }
 
 export default App
-º
