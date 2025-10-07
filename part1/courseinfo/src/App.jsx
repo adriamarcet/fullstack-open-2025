b448510e4535a6f64ac607cbf2d5babe503344a3
Adrià <adriamarcetrovira@gmail.com>
2025-10-07 09:46:27 +0200

    Part 1 exercise 1.8: redactor statistics component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
+  
 import Header from "./Header.jsx";
 import Content from "./Content.jsx";
 import Total from "./Total.jsx";
-
+  
 function App() {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+  
   const course = {
     name: 'Half Stack application development',
     parts: [{
@@ -19,6 +26,32 @@ function App() {
     }]
   };
 
+  function logStatus(updatedLeft, updatedRight, updatedTotal) {
+    console.log(`
+      Left clicks: ${updatedLeft}
+      Right clicks: ${right}
+      Total clicks: ${total}
+      All clicks: ${allClicks}
+    `);
+  }
+
+
+  function handleLeftClick() {
+    setAll(allClicks.concat('L'));
+    
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right) 
+    logStatus(updatedLeft)
+  }
+  
+  function handleRightClick() {
+    setAll(allClicks.concat('R'));
+    const updateRight = right + 1;
+    setRight(updateRight);
+    logStatus(left, updateRight)
+  }
+
   return (
     <>
       <Header course={course.name} />
@@ -26,8 +59,12 @@ function App() {
         parts={course.parts}
       />
       <Total parts={course.parts} />
+      <hr />
+      {left} <button onClick={handleLeftClick}>Left click</button>
+      <button onClick={handleRightClick}>Right click</button> {right}
     </>
   )
 }
 
 export default App
+  
