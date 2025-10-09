44b9a34c59bb8ea135a3c58ec02a466c83dab699
Adrià <adriamarcetrovira@gmail.com>
2025-10-09 15:05:40 +0200

    Part 2 exercices 2.4 and 2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -2,11 +2,13 @@ import Header from './Header';
 import Content from './Content';
 
 const Course = ({course}) => {    
+    console.log('course from course component ', course);
+    
     return (
-        <>
+        <article>
             <Header course={course} />
             <Content course={course} />
-        </>
+        </article>
     )
 }
 
