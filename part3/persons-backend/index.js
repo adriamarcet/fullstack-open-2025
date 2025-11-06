7f65478a584cb89a53f76d4711b6e70aaa8f66fe
Adrià <adriamarcetrovira@gmail.com>
2025-11-06 14:44:39 +0100

    Part 3 exercise 3.8*: Phonebook backend step 8 - after review

--- part3/persons-backend/index.js
+++ part3/persons-backend/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(morgan('tiny'));
 
 morgan.token('tellme', function (req, res) {
     const requestHost = JSON.stringify(req.host);
