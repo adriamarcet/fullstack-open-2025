5aeff3e264492fed0f8740d7601384dee007d004
Adrià <adriamarcetrovira@gmail.com>
2025-11-12 10:57:22 +0100

    Part 3 exercise 3.12

---
+++ part3/persons-backend/mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema);
+
+if(process.argv.length < 3) {
+    console.log('give password as argument');
+    process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://adriamarcetrovira_db_user:${password}@cluster0.xcuptqd.mongodb.net/characterApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Connection error:', err));
+
+
+const givenName = process.argv[3];
+const givenNumber = process.argv[4];
+
+if(!givenName && !givenNumber) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+        return;
+    })
+
+    return;
+}
+
+const person = new Person({
+    name: givenName,
+    number: givenNumber
+})
+
+person.save().then(result => {
+    console.log('is saved');
+    mongoose.connection.close()
+})
