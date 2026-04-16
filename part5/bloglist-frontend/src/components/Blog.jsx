5bebe1bed7133f5ff836681dbf9e943157eafd10
Adrià Marcet <adriamarcetrovira@gmail.com>
2026-04-16 14:20:14 +0200

    Add handleLike feature to blog component

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,8 +1,33 @@
-const Blog = ({ blog }) => (
-  <div className="blog-item">
-    <div className="blog-title">{blog.title}</div>
-    <div className="blog-author">{blog.author}</div>
-  </div>
-)
+import { useState, useEffect } from 'react'
+import Toggable from "./Toggleble"
+import blogService from '../services/blogs'
+
+const Blog = ({ blog, likeFn }) => {
+
+  const increaseLike = () => {
+    likeFn(blog)
+  }
+
+  return (
+    <div className="blog-item">
+      <p className="blog-title">
+        <span>{blog.title}</span>
+        {blog.author !== null && (
+          <span> by <span className="blog-author">{blog.author}</span></span>
+        )}
+      </p>
+      
+      <Toggable buttonLabel="View more details">
+        <div>
+          <ul>
+            <li>URL: {blog.url}</li>
+            <li>Likes: {blog.likes} <button onClick={increaseLike}>Like</button></li>
+            {blog.user !== null && <li>Entered by: {blog.user.name}</li>}
+          </ul>
+        </div>
+      </Toggable>
+    </div>
+  )
+}
 
 export default Blog
